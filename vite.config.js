a813610539f6523d12a3e1d1ca50c2d97075ed16
User <user@MacBook-Air.local>
2026-06-19 16:05:10 +0600

    попытка исправть ошибку

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Chip-Dale/', // <-- Это скажет Vite, как правильно собирать пути к картинкам и скриптам
 })
