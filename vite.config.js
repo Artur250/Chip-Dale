772382564ba700c30596afeb789426d1138c40ed
Artur250 <gameartur012@gmail.com>
2026-08-16 14:52:39 +0600

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Chip-Dale/', // <-- Это скажет Vite, как правильно собирать пути к картинкам и скриптам
+  base: '/Chip-Dale/',
 })
