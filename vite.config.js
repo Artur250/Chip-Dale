96ec002eef5ef15f382c7083a1a687c3e4c7e085
Artur250 <gameartur012@gmail.com>
2026-08-16 14:58:19 +0600

    Add CNAME for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Chip-Dale/',
+  base: '/', // <-- Укажите просто слэш для кастомного домена
 })
